f61faf57657d6794e1b48d2fce83bb74efb2bade
tusharswami534 <tusharswami17@gmail.com>
2024-12-03 09:32:44 +0530

    faq done

--- src/components/home/CustomAccordion.jsx
+++ src/components/home/CustomAccordion.jsx
@@ -14,10 +14,10 @@ const CustomAccordion = () => {
       <div className='max-w-[590px] mx-auto'>
         <div className='flex flex-col gap-2'>
         {ACCORDION_DATA.map((data, i) => (
-            <div key={i} className='overflow-hidden min-h-[20px]'>
+            <div key={i} className='overflow-hidden min-h-[50px]'>
             <button
               onClick={() => toggleAccordion(i)}
-              className='w-full p-2 bg-red-200 opacity-100 justify-between items-center flex font-bold text-3xl'>
+              className='w-full p-2 bg-red-200 opacity-100 justify-between relative z-10 items-center flex font-bold text-3xl'>
               {data.ButtonName}
               <img
                 width={'30px'}
@@ -26,9 +26,11 @@ const CustomAccordion = () => {
                 alt="uparrow"
               />
             </button>
-            <p className={`${openIndex === i ? 'max-h-full duration-300 ease-linear p-2 bg-black' : 'h-0 duration-300 ease-linear p-0'} overflow-hidden text-white`}>
-              {data.Content}
+            <div className={`${openIndex === i ? 'm-0 max-h transition-all ease-linear duration-300 p-2' : '-mt-10 max-h-0 transition-all ease-linear duration-500'} overflow-hidden text-white bg-black`}>
+            <p >
+              This is the accordion panel content.
             </p>
+            </div>
           </div>
         ))}
         </div>
